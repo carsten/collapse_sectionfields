a1999b1a52b46c53e313e614d05aa02a0226d8f9
michael-e <info@michael-eichelsdoerfer.de>
2010-02-25 20:43:34 +0800

    Added German language file.

---
+++ lang/lang.de.php
@@ -0,0 +1,27 @@
+<?php
+
+	$about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Michael Eichelsdoerfer',
+			'email' => 'info@michael-eichelsdoerfer.de',
+			'website' => 'http://www.michael-eichelsdoerfer.de'
+		),
+		'release-date' => '2010-02-25'
+	);
+	
+	
+	/*
+	 * EXTENSION: Collapse Sectionfields
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+	
+		'Collapse' =>
+		'Zusammenklappen',
+
+		'Uncollapse' =>
+		'Ausklappen',
+
+	);
